3c6575b01d897b155534e410fdf32d36a4628b69
Cata Álamos <cataalamos@Mac.lan>
2025-02-21 19:19:50 -0500

    Initial commit for Streamlit app

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+
+st.title("Hello, Streamlit!")
+st.write("This is a test to check if Streamlit is working.")
+
+if st.button("Click Me"):
+    st.write("Button Clicked! Streamlit is running 🚀")
